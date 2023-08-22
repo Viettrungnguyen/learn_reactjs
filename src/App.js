7ebfd16cf6c2694356118548ec62597798eaee75
Trung Nguyen <trung.nguyen@kiaisoft.com>
2023-08-22 16:15:08 +0700

    react router v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import News from "./pages/News";
 import "./App.css";
-import Button from "./components/Button";
 
 function App() {
   return (
-    <div style={{ padding: "0 32px" }}>
-      <Button primary />
-      <Button />
-      <Button disable />
+    <div className="app">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
