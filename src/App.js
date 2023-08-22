950c9cedb61ea14534c07edfe8c2e970fe1f0d74
Trung Nguyen <trung.nguyen@kiaisoft.com>
2023-08-22 13:44:40 +0700

    Sử dụng css trong react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import { useEffect, useRef } from "react";
-// import { useImperativeHandle } from "react";
-import Video from "./video";
+// 1. inline css
+// 2. App.css
+// 3. Chia component
+import "./App.css";
 
 function App() {
-  const videoRef = useRef();
-
-  useEffect(() => {
-    console.log(videoRef.current);
-  });
-
-  const handlePlay = () => {
-    videoRef.current.play();
-  };
-
-  const handlePause = () => {
-    console.log(videoRef.current);
-    videoRef.current.pause();
-  };
-
   return (
-    <div>
-      <Video ref={videoRef} />
-      <button onClick={handlePlay}>Play</button>
-      <button onClick={handlePause}>Pause</button>
+    <div style={{ padding: "0 32px" }}>
+      <h1 className="heading">CSS</h1>
     </div>
   );
 }
 
 export default App;
+
+// Development: npm start / yarn start --> CSS internal
+// Production: npm run build / yanr build --> CSS external
---
+++ src/components/Heading/index.js
@@ -0,0 +1,7 @@
+import "./Heading.css";
+
+function Heading() {
+  return <h1 className="heading">Heloooo</h1>;
+}
+
+export default Heading;
